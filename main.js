5cd3ed248787f74a0c048a5732b10ea222a858f4
Katie Williams <kawilliams8@gmail.com>
2019-03-21 21:24:22 -0600

    Add initial random number and win/lose logic with responding messages

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+//Global variables
 var minRangeInput = document.getElementById("min-range-input");
 var maxRangeInput = document.getElementById("max-range-input");
+var minRangeParsed = parseInt(minRangeInput);
+var maxRangeParsed = parseInt(maxRangeInput);
 var minRangeDisplay = document.getElementById("low-range-display")
 var maxRangeDisplay = document.getElementById("high-range-display")
 var updateRangeButton = document.getElementById("update-range-button");
@@ -14,27 +17,37 @@ var resetButton = document.getElementById("reset-button");
 var clearButton = document.getElementById("clear-button");
 var challengerOneGuessDisplay = document.getElementById("challenger-one-current-guess");
 var challengerTwoGuessDisplay = document.getElementById("challenger-two-current-guess");
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+var challengerOneGuessComparison = document.getElementById("comparison-result-1");
+var challengerTwoGuessComparison = document.getElementById("comparison-result-2");
+//Event listeners
+submitButton.addEventListener("click", submitNamesGuesses);
+// updateRangeButton.addEventListener("click", updateRanges);
+console.log(randomNumber);
 
 //Generate random number
-function randomNumber(minRangeInput,maxRangeInput) {
-    return Math.floor((Math.random() * (maxRangeInput)) + minRangeInput);
-  }
+// function createRandomNumber(num) {
+// 	var random = Math.floor(Math.random() * 100) + 1;
+// 	console.log("local random " + random);
+//   num = random;
+//   }
+//   console.log("global random" + RandomNumber);
 
-  //Get and display user's game ranges
-function updateRanges () {
-  var minRange = minRangeInput.value;
-  var maxRange = maxRangeInput.value;
-  //console.log(minRange,maxRange);
-  randomNumber(minRange,maxRange);
-  minRangeDisplay.innerText = minRange;
-  maxRangeDisplay.innerText = maxRange;
-}
+//Get and display user's game ranges, create the secret number
+// function updateRanges(minRangeParsed, maxRangeParsed) {
+//   var minRange = minRangeParsed.innerText;
+//   console.log(minRange);
+//   var maxRange.innerText = maxRangeParsed.innerText;
+//   var random = Math.floor((Math.random() * (100)) + 1);
+//   console.log(random);
+//   minRangeDisplay.value = minRange;
+//   maxRangeDisplay.value = maxRange;
+// }
 
-var el = updateRangeButton;
-el.onclick = updateRanges;
 
 //Get names/guesses, then display names/guesses
 function submitNamesGuesses(){
+  checkGuesses();
   var nameOne = challengerOneNameInput.value;
   var guessOne = challengerOneGuessInput.value;
   var nameTwo = challengerTwoNameInput.value;
@@ -47,20 +60,37 @@ function submitNamesGuesses(){
   challengerTwoGuessDisplay.innerText = guessTwoDisplay;
 }
 
-var el = submitButton;
-el.onclick = submitNamesGuesses;
+function checkGuesses() {
+    if (challengerOneGuessInput.value == randomNumber && 
+      challengerTwoGuessInput.value == randomNumber) {
+      challengerOneGuessComparison.innerText = "It's a tie!";
+      challengerTwoGuessComparison.innerText = "It's a tie!";
+  } else if(challengerOneGuessInput.value == randomNumber) {
+      challengerOneGuessComparison.innerText = "Boom!";
+  } else if (challengerTwoGuessInput.value == randomNumber) {
+      challengerTwoGuessComparison.innerText = "Boom!";
+  } else if (challengerOneGuessInput.value > randomNumber && 
+      challengerTwoGuessInput.value > randomNumber) {
+      challengerOneGuessComparison.innerText = "that's too high";
+      challengerTwoGuessComparison.innerText = "that's too high";
+  } else if (challengerOneGuessInput.value > randomNumber && 
+      challengerTwoGuessInput.value < randomNumber) {
+      challengerOneGuessComparison.innerText = "that's too high";
+      challengerTwoGuessComparison.innerText = "that's too low";
+  } else if (challengerOneGuessInput.value < randomNumber && 
+      challengerTwoGuessInput.value < randomNumber) {
+      challengerOneGuessComparison.innerText = "that's too low";
+      challengerTwoGuessComparison.innerText = "that's too low";
+  } else if (challengerOneGuessInput.value < randomNumber && 
+      challengerTwoGuessInput.value > randomNumber) {
+      challengerOneGuessComparison.innerText = "that's too low";
+      challengerTwoGuessComparison.innerText = "that's too high";
+  } else if (challengerTwoGuessInput.value > randomNumber) {
+      challengerTwoGuessComparison.innerText = "that's too high";
+  } else {
+    return;
+    console.log("no else ifs match");
+  };
+};
 
-//Clear name/guess inputs and displays
-//Use removeEventListener here?
-function resetGameButton() {
-  challengerOneNameInput.value = null;
-  challengerOneGuessInput.value = null; 
-  challengerTwoNameInput.value = null;
-  challengerTwoGuessInput.value = null;
-  //nameDisplayOne.textContent = "Challenger 1 Name";
-  //nameDisplayTwo.innerText = "Challenger 2 Name";
-  randomNumber();
-}
-
-var el = resetButton;
-el.onclick = resetGameButton;
+// window.onload = createRandomNumber();
